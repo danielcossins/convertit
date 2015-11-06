d3915fa4dd6a89b1ce0f61f40b9e9861842884ad
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-05 22:14:56 -0600

    saving commented code that doesn't work

--- scripts/call.js
+++ scripts/call.js
@@ -15,4 +15,16 @@ define(function(require){
     });
     return deferred.promise;
   };
+  // return function(){
+  //   var link;
+  //   var selectedFile = document.getElementById(id).files[0];
+  //   var fd = new FormData(); 
+  //   fd.append("image", selectedFile);
+  //   var xhr = new XMLHttpRequest();
+  //   console.log(xhr);
+  //   xhr.open("POST", "https://api.imgur.com/3/image.json");
+  //   xhr.setRequestHeader('Authorization', 'Client-ID 4ff4d561a18ac75');
+  //   xhr.send(fd);
+  //   return xhr;
+  // };
 });
--- scripts/main.js
+++ scripts/main.js
@@ -19,13 +19,30 @@ requirejs.config({
 
 requirejs(["call"], 
   function(call){
-    var upload = $('#upload').val();
-    console.log($('#go'));
     $('#go').on("click", function(){
-      var promise = call("https://api.cloudconvert.com/convert?input="+upload+"&download=inline");
+      var upload = document.getElementById('upload').files[0];
+      console.log(upload);
+      // var upload = document.getElementById('upload').value;
+      // var upload = document.getElementById('upload').value.replace("C:\\fakepath\\", "");
+      // console.log(upload);
+      // // var promise = call("https://api.cloudconvert.com/convert?input="+upload+"&download=inline&apikey=ggZ9GdN_8u9Yduz2uK7kI4H1OoBuE0XokQeMZSLiImG98d5FlmYtoPg2D4R34unbm4yF85TXncF1SPTz6W4iAA&inputformat=png&outputformat=jpg");
+      // var promise = call("https://api.cloudconvert.com/convert?apikey=ggZ9GdN_8u9Yduz2uK7kI4H1OoBuE0XokQeMZSLiImG98d5FlmYtoPg2D4R34unbm4yF85TXncF1SPTz6W4iAA&download=true&inputformat=png&outputformat=jpg&input=" + upload);
+      // // var promise = call("https://api.cloudconvert.com/convert?apikey=ggZ9GdN_8u9Yduz2uK7kI4H1OoBuE0XokQeMZSLiImG98d5FlmYtoPg2D4R34unbm4yF85TXncF1SPTz6W4iAA&input=upload&download=inline&inputformat=png");
+      // console.log(promise);
       // promise.then(function(data){
       //   console.log(data);
       // });
-      console.log(promise);
+
+
+
+      $.ajax({
+        // url: "https://api.cloudconvert.com/convert?apikey=ggZ9GdN_8u9Yduz2uK7kI4H1OoBuE0XokQeMZSLiImG98d5FlmYtoPg2D4R34unbm4yF85TXncF1SPTz6W4iAA&input=upload&download=inline&inputformat=png&outputformat=jpg&filename=upload&file=" + upload,
+        // url: "https://api.cloudconvert.com/convert?apikey=ggZ9GdN_8u9Yduz2uK7kI4H1OoBuE0XokQeMZSLiImG98d5FlmYtoPg2D4R34unbm4yF85TXncF1SPTz6W4iAA&input=upload&filename=AGmail-Logo.png&download=false&inputformat=png&file=" + upload,
+        url: "https://api.cloudconvert.com/convert?apikey=ggZ9GdN_8u9Yduz2uK7kI4H1OoBuE0XokQeMZSLiImG98d5FlmYtoPg2D4R34unbm4yF85TXncF1SPTz6W4iAA&input=upload&filename=AGmail-Logo.png&download=false&inputformat=png",
+        method: "GET"
+
+        }).done(function(data) {
+          console.log(data);
+      });
     });
 });
